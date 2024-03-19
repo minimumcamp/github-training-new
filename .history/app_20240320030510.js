d34246f3229f173a38688523e3d56ec057e4eac1
minimumcamp <ryo723.pc@gmail.com>
2024-03-20 03:10:59 +0900

    done function done-todo done

---
+++ .history/app_20240320030510.js
@@ -0,0 +1,18 @@
+$('#addTodo').click(function () {
+    const inputTodo = $("input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    $("input").val("");
+});
+
+$(document).on("change", "input[type=checkbox]", function () {
+    if ($(this).is("checked")) {
+        $(this).parent().css("text-decoration", "line-through")
+        $(this).parent().css("color", "#ccc");
+
+    } else {
+        $(this).parent().css("text-decoration", "none")
+    }
+
+
+});
